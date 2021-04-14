5be501c90a6f7b42b98bb24fa4636dd0603c0afa
Manu <manuelferat@gmail.com>
2021-04-14 20:02:00 +0200

    space shaders

---
+++ vertex_shader_space.glsl
@@ -0,0 +1,13 @@
+uniform mat4 mvpMatrix;
+uniform float xt;
+uniform float yt;
+uniform float zt;
+
+attribute vec3 vertex;
+
+
+void main(void)
+{
+   vertex += vec3(xt,yt,zt);
+   gl_Position = mvpMatrix * vec4(vertex, 1.0);
+}
